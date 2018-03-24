22033bc56e40d03c1ccb3761acd2a4ae3758a028
qays <635216690@qq.com>
2018-03-24 09:25:57 +0800

    add something

---
+++ src/main/java/com/qays/SlugApplication.java
@@ -0,0 +1,12 @@
+package com.qays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlugApplication.class, args);
+	}
+}
---
+++ src/test/java/com/qays/SlugApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlugApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
