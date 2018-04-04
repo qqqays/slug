300216a1577685908de670bf0a8d0ec7805e2d34
Qays_ <635216690@qq.com>
2018-04-04 16:04:15 +0800

    add function of statistics keywords

--- src/test/java/com/qays/SlugApplicationTests.java
+++ src/test/java/com/qays/SlugApplicationTests.java
@@ -25,63 +25,68 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SlugApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
-	@Test
-	public void httpclientTest(){
-		CloseableHttpClient httpClient = HttpClients.createDefault();
+    @Test
+    public void httpclientTest() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
 //		HttpGet httpGet = new HttpGet("http://www.baidu.com/s?ie=UTF-8&wd=太阳能电池");
-		HttpGet httpGet = new HttpGet("http://www.swkj.net.cn/about-sw.html");
+        HttpGet httpGet = new HttpGet("http://www.swkj.net.cn/about-sw.html");
 //		HttpGet httpGet = new HttpGet("http://www.swkj.net.cn");
 //		HttpGet httpGet = new HttpGet("http://www.swpv.net");
 
-		try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
 
-			int statusCode = response.getStatusLine().getStatusCode();
-			if (statusCode != HttpStatus.SC_OK) {
-				System.err.println("Method failed:" + response.getStatusLine());
-			} else {
-				String resultStr = EntityUtils.toString(response.getEntity());
-				System.out.println("result:" + resultStr);
-			}
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != HttpStatus.SC_OK) {
+                System.err.println("Method failed:" + response.getStatusLine());
+            } else {
+                String resultStr = EntityUtils.toString(response.getEntity());
+                System.out.println("result:" + resultStr);
+            }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Test
-	public void jsoupTest(){
+
+    //	statistics links
+    @Test
+    public void jsoupTest() {
 //		Connection con = Jsoup.connect("http://www.swpv.net");
-//		Connection con = Jsoup.connect("http://www.sctaiyi.com/index.html");
-		Connection con = Jsoup.connect("http://www.jhzm88.com/");
+        Connection con = Jsoup.connect("http://www.sctaiyi.com/index.html");
+//		Connection con = Jsoup.connect("http://www.jhzm88.com/");
+//		Connection con = Jsoup.connect("http://www.jsgangyao.com");
 
-		con.header(
-				"User-Agent",
-				"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
-		);
+        con.header(
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
+        );
 
-		Document doc = null;
-		try {
-			doc = con.get();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        Document doc = null;
+        try {
+            doc = con.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-		Elements links = doc.getElementsByTag("a");
+        Elements links = doc.getElementsByTag("a");
 
-		int i = 0;
-		for (Element link : links) {
-			System.out.println(link.attr("abs:href") + " 链接名：" + link.text() + " numbers: " + i++);
-		}
+        int i = 0;
+        for (Element link : links) {
+            System.out.println(link.attr("abs:href") + " 链接名：" + link.text() + " numbers: " + i++);
+        }
 
 //		Elements contents = doc.getElementsByTag("p");
 //
@@ -92,54 +97,87 @@ public class SlugApplicationTests {
 //
 //		System.out.println("number" + number);
 
-	}
+    }
 
-	@Test
-	public void strLengthTest(){
-		String a = "发电";
-		String b = "abc";
+    //	statistics keywords
+    @Test
+    public void keywords() {
 
-		System.out.println(a.length());
-		System.out.println(b.length());
-	}
+        Connection con = Jsoup.connect("http://www.jsgangyao.com");
 
-	@Test
-	public void setTest(){
-		Set<String> set = ConcurrentHashMap.newKeySet();
+        con.header(
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
+        );
 
-		set.add("a");
-		System.out.println(set.add("b"));
-		System.out.println(set.add("b"));
-		set.add("c");
-	}
+        Document doc = null;
+        try {
+            doc = con.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	@Test
-	public void methodTest(){
+        Element body = doc.body();
 
-		GainLinks gainLinks = new GainLinks(ConcurrentHashMap.newKeySet(), new ConcurrentLinkedQueue<>());
+        Pattern p = Pattern.compile("太阳能路灯");
 
-		Runnable run = () -> gainLinks.operate("http://www.swpv.net");
+        Matcher m = p.matcher(body.text());
 
-		Thread thread = new Thread(run);
+        int count = 0;
+        while (m.find()) {
+            count++;
+        }
 
-		thread.start();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~The keywords of this page are : " + count);
 
-		System.out.println("finish");
+    }
 
-	}
+    @Test
+    public void strLengthTest() {
+        String a = "发电";
+        String b = "abc";
 
-	@Test
-	public void threadPool(){
-		ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
+        System.out.println(a.length());
+        System.out.println(b.length());
+    }
+
+    @Test
+    public void setTest() {
+        Set<String> set = ConcurrentHashMap.newKeySet();
+
+        set.add("a");
+        System.out.println(set.add("b"));
+        System.out.println(set.add("b"));
+        set.add("c");
+    }
+
+    @Test
+    public void methodTest() {
+
+        GainLinks gainLinks = new GainLinks(ConcurrentHashMap.newKeySet(), new ConcurrentLinkedQueue<>());
+
+        Runnable run = () -> gainLinks.operate("http://www.swpv.net");
+
+        Thread thread = new Thread(run);
+
+        thread.start();
+
+        System.out.println("finish");
+
+    }
+
+    @Test
+    public void threadPool() {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
 
 
-		System.out.println(fixedThreadPool.isTerminated());
+        System.out.println(fixedThreadPool.isTerminated());
 
-		while (!fixedThreadPool.isTerminated()) {
+        while (!fixedThreadPool.isTerminated()) {
 //			fixedThreadPool.execute(new MyThreadPool());
-		}
+        }
 
-	}
+    }
 
 
 }
