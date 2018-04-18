a6bc0409bd86a124c07b8c47dea16da1e47bf64a
Qays_ <635216690@qq.com>
2018-04-18 12:12:11 +0800

    add original function for controller

--- src/main/java/com/qays/controller/ProController.java
+++ src/main/java/com/qays/controller/ProController.java
@@ -32,7 +32,7 @@ public class ProController {
     )
     @ApiImplicitParams({
             @ApiImplicitParam(name = "urls", value = "array of url group", required = true, paramType = "query"),
-            @ApiImplicitParam(name = "option", value = "specify param", required = true, paramType = "query"),
+            @ApiImplicitParam(name = "option", value = "name, class or id of tag", required = true, paramType = "query"),
             @ApiImplicitParam(name = "seed", value = "which website", required = true, paramType = "query")
     })
     @PostMapping("/refine")
@@ -49,7 +49,7 @@ public class ProController {
     @ApiOperation(value = "Gets links of whole website", notes = "ditto")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "urls", value = "array of url group", required = true, paramType = "query"),
-            @ApiImplicitParam(name = "option", value = "specify param", required = true, paramType = "query"),
+            @ApiImplicitParam(name = "option", value = "ignore", paramType = "query"),
             @ApiImplicitParam(name = "seed", value = "which website", required = true, paramType = "query")
     })
     @GetMapping("/getLinks")
@@ -95,5 +95,37 @@ public class ProController {
     @GetMapping("/keywordsOfPage")
     public Integer keywordsOfPage(@RequestParam String url, @RequestParam String keywords){
         return crawler.keywordOfPage(url, keywords);
+    }
+
+    /**
+     * Counting the keywords of whole website
+     * @param cpc
+     * @return
+     */
+    @ApiOperation(value = "Counting keywords of whole website")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "urls", value = "restrict prefix of target url", required = true, paramType = "query"),
+            @ApiImplicitParam(name = "option", value = "The keywords that you want to search", required = true, paramType = "query"),
+            @ApiImplicitParam(name = "seed", value = "The index of the website", required = true, paramType = "query")
+    })
+    @GetMapping("/keywordsOfWebsite")
+    public Integer keywordsOfWebsite(ConstParam4Crawler cpc){
+        return crawler.keywordsOfWebsite(cpc);
+    }
+
+    /**
+     * Counting the number of img tag, img alt and keywords matched alt
+     * @param cpc
+     * @return
+     */
+    @ApiOperation(value = "Counting the number of img tag, img alt and which keywords match alt")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "urls", value = "restrict prefix of target url", required = true, paramType = "query"),
+            @ApiImplicitParam(name = "option", value = "The keywords that you want to match img alt", required = true, paramType = "query"),
+            @ApiImplicitParam(name = "seed", value = "The home of the website", required = true, paramType = "query")
+    })
+    @GetMapping("/imgAltOfWebsite")
+    public String imgAltOfWebsite(ConstParam4Crawler cpc){
+        return crawler.imgAltOfWebsite(cpc);
     }
 }
